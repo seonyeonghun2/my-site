3d3888ecede8bff679d1513e099f416137e94ac5
seonyeonghun2 <kjca1117@naver.com>
2025-04-23 17:40:48 +0900

    정보수정 라우트 컴포넌트
    1. 쿠키에서 jwt 토큰 파싱
    
    2. jwt decode에서 기본 formData 스테이트 : 초기화
    
    3. useEffect 훅 : 폼의 defaultValue 설정을 위한 setFormData 수정함수를 호출, jwt를 파싱한 decode의 객체정보중에 사용자명, 연락처, 이메일 정보를 초기화
    
    4. input 에 defaultValue 속성을 추가, 폼의 기본값 렌더링
    
    5. 폼에서 데이터 수정후 put 요청을 할 경우, formData를 구조분해한 뒤 sendData 객체로 id값을 동봉해서 서버에 요청
    
    6. 수정이 정상적으로 처리되면, alert() 호출하고 useNavigate 훅을 이용해 처음화면으로 라우팅 처리

--- client/src/routes/Modify.jsx
+++ client/src/routes/Modify.jsx
@@ -1,65 +1,110 @@
-import {useState} from 'react'
-import axios from 'axios'
-import jsCookie from 'js-cookie'
-import {jwtDecode} from 'jwt-decode'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import jsCookie from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate} from 'react-router'
 function Modify() {
-    const cookie = jsCookie.get('auth_token')
+  const navigate = useNavigate()
+  const [userInfo, setUserInfo] = useState({});
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+  });
+  useEffect(() => {
+    const cookie = jsCookie.get('auth_token');
     if (!cookie) {
-        alert('정보가 없습니다.')
-    } 
-    const decoded = jwtDecode(cookie)    
-    
-    const [formData, setFormData] = useState({        
-        name: '',
-        pwd: '',
-        phone: '',
-        email: ''
+      alert('정보가 없습니다.');
+    }
+    const decoded = jwtDecode(cookie);
+    // console.log(decoded)
+    setUserInfo(decoded)
+    setFormData({
+      name: decoded.user_name,
+      phone: decoded.user_phone,
+      email: decoded.user_email
+    })
+  }, []);
+  console.log(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const sendData = {
+      ...formData,
+      id: userInfo.user_id,
+    };
+    // console.log(sendData);
+    axios
+      .put('http://localhost:4000/api/user', sendData, {
+        withCredentials: true,
       })
-      const handleSubmit = (e) => {
-        e.preventDefault()    
-        const sendData = {
-            ...formData,
-            id: decoded.id
+      .then(function (response) {
+        if(response.status === 200 || response.statusText === 'OK') {
+          alert("정보 수정 완료! \n 첫페이지로 이동합니다.")
+          navigate('/')
         }
-        console.log(sendData);
-        // axios.post('http://localhost:4000/api/user', formData)
-        // .then(function (response) {
-        //   console.log(response);
-        // })
-        // .catch(function (error) {
-        //   console.log(error);
-        // });
-      }
-      const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value
-        })
-      }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <form onSubmit={handleSubmit}>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 min-w-7xl mx-auto">
         <legend className="fieldset-legend">Modify</legend>
 
-        <label className="label text-2xl">password</label>
-        <input type="password" className="input min-w-full" placeholder="password" name="pwd" id="pwd" onChange={handleChange} />
-
         <label className="label text-2xl">name</label>
-        <input type="text" className="input min-w-full" placeholder="name" name="name" id="name" onChange={handleChange} />
+        <input
+          type="text"
+          className="input min-w-full border border-black"
+          placeholder="name"
+          name="name"
+          id="name"
+          disabled
+          onChange={handleChange}
+          defaultValue={userInfo.user_name}
+        />
         <label className="label text-2xl">phone</label>
-        <input type="tel" className="input min-w-full" placeholder="phone number" name="phone" id="phone" onChange={handleChange} />
+        <input
+          type="tel"
+          className="input min-w-full"
+          placeholder="phone number"
+          name="phone"
+          id="phone"
+          onChange={handleChange}
+          defaultValue={userInfo.user_phone}
+        />
         <label className="label text-2xl">email</label>
-        <input type="email" className="input min-w-full" placeholder="email address" name="email" id="email" onChange={handleChange} />
+        <input
+          type="email"
+          className="input min-w-full outline-0"
+          placeholder="email address"
+          name="email"
+          id="email"
+          onChange={handleChange}
+          defaultValue={userInfo.user_email}
+        />
         <button className="btn bg-white text-black border-[#e5e5e5]" type="submit">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-</svg>
-
-  confirm
-</button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+          </svg>
+          confirm
+        </button>
       </fieldset>
     </form>
-  )
+  );
 }
 
-export default Modify
+export default Modify;
