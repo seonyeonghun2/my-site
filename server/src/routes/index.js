bc363cdaf8f62e5811950152a0c792233a12422f
seonyeonghun2 <kjca1117@naver.com>
2025-04-23 15:15:01 +0900

    user 라우터
    1. 로그인 : /signin 라우터 작성

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import Router from '@koa/router';
-
 const api = new Router();
+
 import userCtrl from '../controllers/user.js';
+
 api.post('/user', userCtrl.addUser)
 // api.put('/user/:id', userCtrl.updateUser)
 api.put('/user', userCtrl.updateUser)
+api.post('/signin', userCtrl.signIn)
 
 export default api
