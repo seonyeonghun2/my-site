66e000cbe0a165e24b4ad3b7a85fce193aa17390
seonyeonghun2 <kjca1117@naver.com>
2025-04-23 15:14:34 +0900

    user 컨트롤러 추가
    1. signIn : 회원 로그인 및 토큰 생성 및 쿠키 발급 (작성중)

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,5 +1,6 @@
 import knex from '../config/knexfile.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userCtrl = {
     addUser: async (ctx) => {    
         try {
@@ -68,6 +69,48 @@ const userCtrl = {
         ctx.body = {
             msg: "회원정보가 수정되었습니다."
         }
+    },
+    signIn: async (ctx) => {
+        // id 또는 pwd 유무 체크
+        const {id, pwd} = ctx.request.body
+
+        if(!id || !pwd) {
+            ctx.status = 400
+            ctx.body = {
+                msg: "아이디 또는 비밀번호가 누락되었습니다."
+            }
+            return
+        }
+
+        // id와 일치하는 유저를 찾고,
+        const existUser = await knex('users').where({id: id})
+        console.log(existUser)
+        if(existUser.length < 1) {
+            ctx.status = 400
+            ctx.body = {
+                msg: "회원정보가 존재하지 않습니다."
+            }
+            return
+        }
+        
+        // 찾은 유저의 해시된 비밀번호와, body로 전달된 비밀번호를 비교
+        const isMatch = await bcrypt.compare(pwd, existUser[0].pwd)
+
+        if(!isMatch) {
+            ctx.status = 400
+            ctx.body = {
+                msg: "비밀번호가 일치하지 않습니다."
+            }
+        }
+
+        // jwt : 토큰 발급
+        jwt.sign({
+            user_id: existUser[0].id
+        },process.env.JWT_SECRET, {
+            expiresIn: '1d',            
+        })
+
+        // cookie를 생성 --> 응답 : 응답헤더에 기록되어 전송
     }
     
 }
