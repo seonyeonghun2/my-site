10dfa8e7605524db96eeb746abb1bfd2f82f5e2e
seonyeonghun2 <kjca1117@naver.com>
2025-04-24 17:58:42 +0900

    블로그(글) 라우트 컴포넌트
    1. 데이터 요청 : 서버에 요청
    
    2. 리액트 렌더링
    
    3. 로딩 여부 처리 : 데이터 페칭 후 false
    
    4. 날짜변환

--- client/src/routes/Blog.jsx
+++ client/src/routes/Blog.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 function Blog() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const getBlogPosts = () => {
+      axios.get("http://localhost:4000/blog/read").then((response) => {
+        setPosts(response.data.data)
+        setIsLoading(false)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+    getBlogPosts();
+  }, [])
+  if (isLoading) {
+    return (
+      <div>로딩중..</div>
+    )
+  }
+  function stringToDate(dateString) {
+    return new Date(dateString).toLocaleDateString("ko-kr", {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit'
+    }).split(".")
+  }
   return (
-    <div>Blog</div>
+    <div>
+      {
+        posts.map(post => (
+          <div key={post.id} className='my-5'>
+            <h1 className='text-3xl font-extrabold'>{post.title}</h1>
+            <p className='text-2xl my-2'>
+              <span>{stringToDate(post.wr_date)}</span>
+              <span> &#47; </span>
+              <em className='mr-3 text-gray-400'>{post.categories}</em>
+              {/* <span>{post.categories.map(cate => (
+                <em className='mr-3 text-gray-400'>{cate}</em>
+              ))}</span> */}
+            </p>
+            <p>{post.content}</p>
+            <hr className='my-5 border-gray-300'/>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
