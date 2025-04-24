0938d361f7ae3886ac236169b7a51facbda39ac4
seonyeonghun2 <kjca1117@naver.com>
2025-04-24 17:59:53 +0900

    Koa 서버 설정
    1. 라우터 추가 및 연결 (blog, works)

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,8 @@ import Router from '@koa/router'
 import bodyParser from '@koa/bodyparser'
 const router = new Router()
 import api from './routes/index.js'
+import blog from './routes/blog.js'
+import works from './routes/works.js'
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
@@ -14,6 +16,8 @@ app.use(logger('dev'))
 app.use(bodyParser())
 
 router.use("/api", api.routes());
+router.use("/blog", blog.routes());
+router.use("/works", works.routes());
 app.use(router.routes()).use(router.allowedMethods())
 
 
