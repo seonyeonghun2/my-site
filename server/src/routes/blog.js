d95dd110ec3733474eb6998ee84e56e23e622ffc
seonyeonghun2 <kjca1117@naver.com>
2025-04-24 18:02:09 +0900

    blog 라우터
    1. CRUD
    
    2. 전체 get, 부분 get

---
+++ server/src/routes/blog.js
@@ -0,0 +1,11 @@
+import Router from '@koa/router'
+import blogCtrl from '../controllers/blog.js';
+const blog = new Router()
+
+blog.post('/create', blogCtrl.create)
+blog.get('/read', blogCtrl.read)
+blog.get('/post/:postId', blogCtrl.singlePost)
+blog.put('/update', blogCtrl.update)
+blog.delete('/delete', blogCtrl.delete)
+
+export default blog;
