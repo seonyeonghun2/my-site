b3187932ddd28a9505a26e0b901b0a20b8a6606e
seonyeonghun2 <kjca1117@naver.com>
2025-04-25 18:04:38 +0900

    환경변수 적용
    1. DB 연결정보 env 설정

--- server/src/config/knexfile.js
+++ server/src/config/knexfile.js
@@ -1,12 +1,13 @@
+import 'dotenv/config'
 import Knex from 'knex'
 const knex = Knex({
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      port: 3306,
-      user: 'demouser',
-      password: '1234',
-      database: 'mysite',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
     },
     debug: true
   });
