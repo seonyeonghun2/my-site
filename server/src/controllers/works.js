15a2037218b60b9f6aa35e59b6853685cb6861ce
seonyeonghun2 <kjca1117@naver.com>
2025-04-28 17:49:32 +0900

    works 컨트롤러
    1. addWork : multer 적용, 파일 업로드 구현
    1-1. @koa/multer 적용시, 입력 폼에서 e.target.value 가 아닌 e.target.files로 ctx가 전달되어야, file 업로드시 파일 정보를 확인할 수 있었음.
    1-2. multer 가 아닌 @koa/multer의 multer 모듈을 이용해 diskStorage를 정의해야 req.pipe not a function 에러가 발생하지 않았음.
    2. readWorks : knex를 이용한 innerJoin() 처리 및 데이터 응답
    ※ koa-static 패키지를 추가해서, 미들웨어로 장착해야 함.
    ※ upload된 이미지 데이터는 public 으로 static files serving 하도록 처리함 (expressjs의 static middleware 설정을 참고함)

--- server/src/controllers/works.js
+++ server/src/controllers/works.js
@@ -1,13 +1,74 @@
-import knex from "../config/knexfile.js"
 import 'dotenv/config'
+import knex from "../config/knexfile.js"
 import jwt from 'jsonwebtoken'
+import {fileURLToPath} from 'url'
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+import fs from 'fs/promises'
+import multer from '@koa/multer'
+const storage = multer.diskStorage({
+  destination: async function (req, file, cb) {
+    console.log(file)
+    const projectFolder = path.join(__dirname, "../../public");
+    await fs.mkdir(projectFolder, { recursive: true });
+    cb(null, projectFolder);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage, limits: 5 * 1024 * 1024});
+
 const worksCtrl = {
     addWork: async (ctx) => {
-        console.log(ctx.request.headers.authorization)
-        console.log(ctx.request.body)
-
         // multer , @koa/multer를 이용해서~! 파일/work 데이터를 업로드!
+        try {
+            await upload.array('files', 3)(ctx, async () => {
+                const {title, description, is_active} = ctx.request.body                
+                const files = ctx.request.files                
+                //1. works 테이블에 데이터 삽입
+                let workId;
+                workId = await knex('works').insert({title, description, is_active})
+
+                if(!files) {
+                    ctx.status = 401
+                    ctx.body = {
+                        msg: '첨부 파일을 등록하고 다시 시도하세요'
+                    }
+                    return;
+                }
+
+                if(files && files.length > 0) {
+                    // file_id, created_at 컬럼 : auto_increment, now()
+                    const fileArr = files.map((file) => ({
+                        work_id: workId,
+                        file_name: file.filename,
+                        file_path: file.path,
+                        file_type: file.mimetype,
+                        file_size: file.size
+                    }))
+                    await knex('files').insert(fileArr)
+                }
+                ctx.status = 200
+                ctx.body = {
+                    msg: "등록 성공"
+                }
+                //2. files 테이블에 데이터 삽입
+            })
+        } catch (err) {
+            console.error(err)
+        }
         // await knex('works').insert(ctx.request.body)
+    },
+    readWorks: async (ctx) => {        
+        const lists = await knex.from('works').innerJoin('files', 'works.work_id', 'files.work_id').limit(3).orderBy("created_at", "desc");
+        console.log(lists)
+        ctx.status = 200
+        ctx.body = {
+            msg: 'work list를 리턴합니다.',
+            data: lists
+        }
     },
     viewWork: async (ctx) => {
     },
