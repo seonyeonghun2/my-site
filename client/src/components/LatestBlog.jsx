7d8e142de524d9e580dcfbe497bfa516419cc289
seonyeonghun2 <kjca1117@naver.com>
2025-04-28 17:30:48 +0900

    블로그 최신글
    1. 목업데이터 삭제
    
    2. 백엔드 요청후 실제 데이터 적용, UI 렌더링
    
    3. view all 링크 경로 수정

--- client/src/components/LatestBlog.jsx
+++ client/src/components/LatestBlog.jsx
@@ -1,46 +1,50 @@
-import {useState} from 'react'
-import {Link} from 'react-router'
-function LatestBlog() {
-    const [posts, setPosts] = useState([
-        {
-            id: 1,
-            title: "Making a design system from scratch",
-            content: "첫번째 블로그 post 입니다",
-            categories: 'daily',
-            wr_id: "user3",
-            wr_date: "2025-04-24"
-        },
-        {
-            id: 2,
-            title: "두번째 post 입니다.",
-            content: "두번째 블로그 post 입니다",
-            categories: 'daily',
-            wr_id: "user3",
-            wr_date: "2025-04-24"
-        }
-    ])
-    // axios.get("http://localhost:4000/blog/latest")
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
+import axios from 'axios'
+function LatestBlog({stringToDate}) {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getBlogPosts = async () => {
+      await axios
+        .get('http://localhost:4000/blog/read')
+        .then((response) => {
+            setPosts(response.data.data)
+            setIsLoading(false)
+        })
+        .catch((err) => console.error(err));
+    };
+    getBlogPosts();
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="min-h-96 w-7xl mx-auto py-5 flex justify-center items-center">
+        <p>로딩중...</p>
+      </div>
+    );
+  }
   return (
     <section className="w-7xl mx-auto py-5">
-        <div className="title-bar flex justify-between py-5 items-center">
-            <h1 className='text-2xl'>Recent Posts</h1>
-            <Link to='/blog/post/read'>view all</Link>
-        </div>
-        <ul className='flex gap-3'>
-            {posts.map(post => (
-                <li key={post.id} className='bg-white flex-1/2 p-3'>
-                    <Link to={`/blog/post/${post.id}`}>
-                        <h1 className="text-3xl font-bold">{post.title}</h1>
-                        <p className='my-3'>
-                            <span>{post.wr_date}</span> &#124; <span className="text-gray-400">{post.categories}</span>
-                        </p>
-                        <p className='text-2xl'>{post.content}</p>
-                    </Link>
-                </li>
-            ))}
-        </ul>
+      <div className="title-bar flex justify-between py-5 items-center">
+        <h1 className="text-2xl">Recent Posts</h1>
+        <Link to="/blog">view all</Link>
+      </div>
+      <ul className="flex gap-3">
+        {posts.map((post) => (
+          <li key={post.id} className="bg-white flex-1/2 p-3">
+            <Link to={`/blog/post/${post.id}`}>
+              <h1 className="text-3xl font-bold">{post.title}</h1>
+              <p className="my-3">
+                <span>{stringToDate(post.wr_date)}</span> &#124;{' '}
+                <span className="text-gray-400">{post.categories}</span>
+              </p>
+              <p className="text-2xl">{post.content}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </section>
-  )
+  );
 }
 
-export default LatestBlog
+export default LatestBlog;
