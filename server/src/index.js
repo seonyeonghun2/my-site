d97af8ada5f45376712a1436e91e7b70456164b8
seonyeonghun2 <kjca1117@naver.com>
2025-04-28 17:38:39 +0900

    koa 백엔드
    1. 절대경로 상수 : __dirname 설정 추가
    
    2. koa-static : Works 업로드 처리 및 클라이언트 요청에 static files 응답

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,36 @@
+import {fileURLToPath} from 'url'
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 import Koa from 'koa'
 const app = new Koa()
 import cors from '@koa/cors'
 import logger from 'koa-morgan'
-import Router from '@koa/router'
 import bodyParser from '@koa/bodyparser'
-const router = new Router()
 import authMiddleware from './middleware/authMiddleware.js'
+import serve from 'koa-static'
+import Router from '@koa/router'
+const router = new Router()
 import api from './routes/index.js'
 import blog from './routes/blog.js'
 import works from './routes/works.js'
+
 app.use(cors({
+    origin: '*',
     credentials: true,
-    origin: 'http://localhost:5173'
 }))
+// console.log(path.join(__dirname, '/uploads'))
+app.use(serve(path.join(__dirname, '../public')));
 app.use(logger('dev'))
 app.use(bodyParser())
 app.use(authMiddleware)
+
 
 // api, blog, works 라우트 미들웨어보다 앞서서 authMiddleware 사용 (=미들웨어 장착)
 router.use("/api", api.routes());
 router.use("/blog", blog.routes());
 router.use("/works", works.routes());
+
 app.use(router.routes()).use(router.allowedMethods())
 
 
