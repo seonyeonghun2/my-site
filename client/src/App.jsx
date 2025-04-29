df652faf464bd2ab80a6e6d2f6a3fe13fe79b860
seonyeonghun2 <kjca1117@naver.com>
2025-04-29 18:02:38 +0900

    App 라우트 컴포넌트
    1. 라우팅 경로 추가 : contacts 관련

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './routes/Home'
 import Works from './routes/Works'
 import Blog from './routes/Blog'
 import ViewPost from './routes/ViewPost'
+import ViewContact from './routes/ViewContact'
 import Contact from './routes/Contact'
 import Modify from './routes/Modify'
 import SignIn from './routes/SignIn'
@@ -23,8 +24,9 @@ function App() {
             <Route path='blog' element={<Blog />} />
             <Route path='blog/write' element={<WriteBlog />} />
             <Route path='/blog/post/:postId' element={<ViewPost />} />
-            <Route path='contact' element={<Contact />} />
-            <Route path='contact/write' element={<WriteContact />} />
+            <Route path='contacts' element={<Contact />} />
+            <Route path='contacts/write' element={<WriteContact />} />
+            <Route path='/contacts/read/:contactId' element={<ViewContact />} />
         </Route>
         <Route path='/modify' element={<Modify />} />
         <Route path='/signin' element={<SignIn />} />
