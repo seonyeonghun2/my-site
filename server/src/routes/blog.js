d9827615f467667106fbc3b80d9a087a5b97ea0b
seonyeonghun2 <kjca1117@naver.com>
2025-04-29 17:54:28 +0900

    blog 라우터
    1. blog post 삭제 요청 : 요청 파라미터 적용

--- server/src/routes/blog.js
+++ server/src/routes/blog.js
@@ -6,6 +6,6 @@ blog.post('/create', blogCtrl.create)
 blog.get('/read', blogCtrl.read)
 blog.get('/post/:postId', blogCtrl.singlePost)
 blog.put('/update', blogCtrl.update)
-blog.delete('/delete', blogCtrl.delete)
+blog.delete('/delete/:postId', blogCtrl.delete)
 
 export default blog;
